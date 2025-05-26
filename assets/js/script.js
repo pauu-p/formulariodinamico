da28cc53b50f7f8fccef695d23015c839361059b
pauu-p <p.delgado@duocuc.cl>
2025-05-25 23:35:59 -0400

    finito

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 $('.contenido-dinamico').hide();
 
+
 document.getElementById('miformulario').addEventListener('submit', function(event){
     event.preventDefault();
 
+  
+    
+
+
     let nombre = document.getElementById('nombre').value
+    let nombreMascota = document.getElementById('nombreMascota').value
     let numero = document.getElementById('numero').value
     let mascota = document.querySelector('input[name="mascotas"]:checked')?.value;
     let color = document.getElementById('color').value;
 
     console.log(nombre , numero , mascota, color)
+
+   
+
+
 
     $('.contenido-dinamico').show();
 //ALERTA
@@ -70,26 +80,53 @@ document.getElementById('miformulario').addEventListener('submit', function(even
         pause: 'hover'
     })
 
+
+
 //OFCANVAS 
-    const myOffcanvas = document.getElementById('myOffcanvas')
-    myOffcanvas.addEventListener('hidden.bs.offcanvas', event => {
-    // do something...
-    })
+    const offcanvasElementList = document.querySelectorAll('.offcanvas')
+    const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
+
+    const offcanvasEl = document.querySelector('.offcanvas');
+offcanvasEl.style.backgroundColor = color; // color es tu variable dinámica
+
 
 //POPOVER
-    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
-    // getOrCreateInstance example
-    const popover = bootstrap.Popover.getOrCreateInstance('#miPopover2') // Returns a Bootstrap popover instance
+    const miPopover = document.getElementById('miPopover');
+    if (miPopover) {
+        const popover = bootstrap.Popover.getOrCreateInstance(miPopover);
 
-    // setContent example
-    popover.setContent({
-    '.popover-header': 'Perrito',
-    '.popover-body': 'Mi perrito es feliz y se llama Ruperto.'
-    })
+        popover.setContent({
+            '.popover-header': mascota ,
+            '.popover-body': `Mi ${mascota} es bonito y se llama ${nombre}`
+        });
+    }
+
 
 //TOAST
-    const toastElList = document.querySelectorAll('.toast')
-    const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, option))
+    const toastElList = document.querySelectorAll('.toast');
+    const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl));
+
+    const toastBtn = document.getElementById('liveToastBtn');
+    toastBtn.textContent = `${nombreMascota} quiere decirte algo`;
+
+    document.getElementById('liveToastBtn').addEventListener('click', function () {
+        const miTostada = document.getElementById('liveToast');
+        const toast = bootstrap.Toast.getOrCreateInstance(miTostada);
+        toast.show();
+
+        document.getElementById('toastTitle').textContent = `🐾 ${nombreMascota}`;
+        document.getElementById('toastBody').innerHTML = `ama me womite ${numero} veces`;
+        let img = document.getElementById('toastImg');
+        let imagenes = imagenesPorMascota[mascota];
+        let imagenToast = imagenes ? imagenes[Math.floor(Math.random() * imagenes.length)] : 'assets/img/default.jpg';
+
+        img.src = imagenToast;
+        img.alt = `Foto de un ${mascota}`;
+        img.className = 'rounded me-2 w-25';
+
+    });
+
 });
